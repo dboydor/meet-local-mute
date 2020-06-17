17c4a8aec422a9514f3ba316740853ddcf3f81e2
David Boyd <david.boyd@sprinklr.com>
2020-06-16 22:15:28 -0700

    - Fixed issue when first installed and setting default stored values - Now have nice new icons

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ muteAll: false });
+    chrome.storage.sync.set({ muteUsers: false });
+    chrome.storage.sync.set({ joined: false });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined,  () => {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
