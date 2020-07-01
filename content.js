145e4dcc9494772a079552cc97261f9703a92366
David Boyd <david.boyd@sprinklr.com>
2020-07-01 13:38:48 -0700

    - New implementation that listens for mutations on the DOM and actively mutes specific speakers

--- content.js
+++ content.js
@@ -1,52 +1,127 @@
 let intervalMute
+let observer
 let people
 let joined = false
+let meeting = {}
+let muted = false
 
 const getUsers = () => {
     return document.querySelectorAll("div[jscontroller] > div[data-self-name]")
 }
 
-// Checks if a user is muted or not
-const isMuted = (user) => {
-    const found = getUsers()
-    for (let x = 0; x < found.length; x++) {
-        const element = found[x]
-        if (element.textContent === user) {
-            const sibling = element.previousElementSibling
-            const found2 = sibling.querySelectorAll("div")
-            for (let y = 0; y < found2.length; y++) {
-                const element = found2[y]
-                if (element.children.length === 3) {
-                    let count = 0
-                    for (let z = 0; z < element.children.length; z++) {
-                        if (element.children[z].nodeName === "DIV") {
-                            count++
+const goUp = (element, count) => {
+    for (let x = 0; x < count; x++) {
+        element = element.parentElement
+    }
+
+    return element
+}
+
+const isSpeaker = (element) => {
+    if (element.childElementCount === 3) {
+        for (let x = 0; x < 3; x++) {
+            const child = element.childNodes[x]
+            if (child.childElementCount !== 0 || child.tagName !== "DIV") {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    return false
+}
+
+// Callback function to execute when mutations are observed
+const watch = (mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === "class") {
+            const target = mutation.target
+            if (isSpeaker(target)) {
+                const rect = target.getBoundingClientRect()
+                if (rect.width !== 0 || rect.height !== 0) {
+
+                    // Person in main screen
+                    let parent = target.parentElement
+                    if (parent && parent.nextElementSibling) {
+                        const sibling = parent.nextElementSibling
+                        const person = sibling.textContent
+                        if (person.length) {
+                            meeting[person] = Date.now()
+                            //console.log("Speaking: ", person)
                         }
                     }
-                    if (count === 3) {
-                        const rect = element.getBoundingClientRect()
-                        if (rect.width === 0 && rect.height === 0) {
-                            //console.log(user + " is muted")
-                            return "muted"
-                        } else {
-                            //console.log(user + " is un-muted")
-                            return "unmuted"
+
+                    // Person in side-bar
+                    parent = goUp(target, 4)
+                    if (parent &&
+                        parent.childNodes.length === 2 &&
+                        parent.childNodes[1].childNodes.length === 2) {
+
+                        const child = parent.childNodes[1].childNodes[0]
+                        const person = child.textContent
+                        if (person.length) {
+                            meeting[person] = Date.now()
+                            //console.log("Speaking: ", person)
                         }
                     }
                 }
             }
         }
     }
+};
 
-    //console.log(user + " not found")
-    return "na"
+const startListening = () => {
+    meeting = {}
+
+    let config = {
+        attributes: true,
+        subtree: true
+    };
+
+    // Create an observer instance linked to the callback function
+    observer = new MutationObserver(watch);
+
+    // Start observing the target node for configured mutations
+    let node = document.querySelector("[data-unresolved-meeting-id]")
+    observer.observe(node, config);
+}
+
+const stopListening = () => {
+    if (observer) {
+        observer.disconnect()
+        observer = null
+    }
 }
 
 // Mutes or unmutes all the audio
 const muteAudio = (mute) => {
-  document.querySelectorAll("audio").forEach(element => {
-    element.muted = mute
-  })
+  if (muted !== mute) {
+    document.querySelectorAll("audio").forEach(element => {
+        element.muted = mute
+    })
+    muted = mute
+  }
+}
+
+const muteSpeakers = (delay) => {
+    const list = Object.keys(meeting)
+    for (let x = 0; x < list.length; x++) {
+        const person = list[x]
+        const elapsed = (Date.now() - meeting[person])
+        const speaking = elapsed <= 1000
+        // if (elapsed > 1000) {
+        //     console.log("elapsed: ", elapsed)
+        // }
+        if (speaking && people.some(person2 => person2.trim() === person.trim())) {
+            console.log(person, " is speaking, muting audio")
+            muteAudio(true)
+            return
+        }
+    }
+
+    console.log("unmuting audio")
+    muteAudio(false)
 }
 
 // Message handelr
@@ -66,12 +141,12 @@ const onMessage = (action, response) => {
                     return
                 }
 
+                startListening()
+
                 people = action.data
                 intervalMute = setInterval(() => {
                     if (people && people.length) {
-                        let unmuted = people.some(person => isMuted(person) === "unmuted")
-                        muteAudio(unmuted)
-                        // console.log("unmuted:" , unmuted, ", ", people)
+                        muteSpeakers(250)
                     }
                 }, 250)
             // Stop muting users
@@ -81,6 +156,7 @@ const onMessage = (action, response) => {
                 }
 
                 if (intervalMute) {
+                    stopListening()
                     muteAudio(false)
                     clearInterval(intervalMute)
                     intervalMute = null
